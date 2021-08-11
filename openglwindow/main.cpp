b4858c509df27205e1dfa46ec45e1b8abb5b92be
madhavpcm <dpmadhav@hotmail.com>
2021-08-11 15:49:03 +0530

    working out smooth curve,not functional

--- openglwindow/main.cpp
+++ openglwindow/main.cpp
@@ -67,12 +67,18 @@ public:
 
     void initialize() override;
     void render() override;
-
+    void getFirstControlPoints();
+    void getCurveControlPoints();
+    std::vector<glm::vec3> updateControlPoints(std::vector<glm::vec3> & rhs);
 private:
     GLint m_posAttr = 0;
     GLint m_colAttr = 0;
     GLint m_matrixUniform = 0;
     GLint m_coord2d = 0;
+
+    std::vector<glm::vec3> m_knots;
+    std::vector<glm::vec3> m_firstControlPoints;
+    std::vector<glm::vec3> m_secondControlPoints;
     QOpenGLShaderProgram *m_program = nullptr;
     int m_frame = 0;
 };
@@ -147,7 +153,7 @@ void TriangleWindow::render()
 
     QMatrix4x4 matrix;
     matrix.perspective(60.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-    matrix.translate(0, 0, -2);
+    matrix.translate(0, 0, -10);
    // matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
 
     m_program->setUniformValue(m_matrixUniform, matrix);
@@ -167,22 +173,16 @@ void TriangleWindow::render()
     static point graph[2000];
 
 
-    static glm::vec3 ctrlpoints[] = {
+    static std::vector<glm::vec3> ctrlpoints = {
             { -4.0, -4.0,0.0}, { -2.0, 4.0,0.0},
             {2.0, -4.0,0.0}, {4.0, 4.0,0.0}
     };
 
     QOpenGLBuffer bezBuffer((QOpenGLBuffer(QOpenGLBuffer::VertexBuffer)));
     bool x=bezBuffer.create();
-    if(!x){
-        //
-        qDebug()<<"not ok";
-    }
+
     x=bezBuffer.bind();
-    if(!x){
-        //
-        qDebug()<<"not ok";
-    }
+
 
     for(int i = 0; i < 2000; i++) {
       float x = (i - 1000.0) / 100.0;
@@ -206,11 +206,10 @@ void TriangleWindow::render()
 
     glDrawArrays(GL_LINE_STRIP, 0, 2000);
    // glDisableVertexAttribArray(m_colAttr);
-    glDisableVertexAttribArray(m_coord2d);
     //bezBuffer.unmap();
     glEnable(GL_MAP1_VERTEX_3);
     glMap1f(GL_MAP1_VERTEX_3,0.0,1.0, 3,4,&ctrlpoints[0][0]);
-    glColor3f( 1.0, 1.0, 1.0 );
+    glColor3f( 1.0, 1.0, 0.0 );
     glBegin(GL_LINE_STRIP);
         for(int i=0; i < 1000; i++)
             glEvalCoord1f((GLfloat) i/ 1000.0);
@@ -220,12 +219,95 @@ void TriangleWindow::render()
     glPointSize(5.0);
     glBegin(GL_POINTS);
          for(glm::vec3 v : ctrlpoints)
-            glVertex3f(v[0],v[1],v[2]);
+            glVertex3f(v.x,v.y,v.z);
     glEnd();
 
+    glDisableVertexAttribArray(m_coord2d);
     bezBuffer.release();
     m_program->release();
 
     ++m_frame;
 }
+
+void TriangleWindow::getCurveControlPoints(){
+    if(!m_knots.size()){
+        qDebug()<<"knots is empty"; return;
+    }
+    if(m_knots.size() < 2){
+        qDebug() << "knots needs at least 2 points"; return;
+    }
+    if(m_knots.size() == 2){
+        m_firstControlPoints.emplace_back(glm::vec3{
+                                              (2 *m_knots[0].x + m_knots[1].x )/3,
+                                              (2 *m_knots[0].y + m_knots[1].y )/3,
+                                               0.0
+                                          });
+        m_secondControlPoints.emplace_back(glm::vec3{
+                                              (2*m_firstControlPoints[0].x - m_knots[0].x),
+                                               (2*m_firstControlPoints[0].y - m_knots[0].y),
+                                               0.0
+                                           });
+        return;
+    }
+    size_t n=m_knots.size()-1;
+    std::vector<glm::vec3> rhs(m_knots.size()-1);
+
+            // Set right hand side X values
+    for (size_t i = 1; i <n-1; ++i){
+        rhs[i].x = 4 * m_knots[i].x + 2 * m_knots[i + 1].x;
+        rhs[i].y = 4 * m_knots[i].y + 2 * m_knots[i + 1].y;
+        rhs[i].z=0;
+
+    }
+    rhs[0].y = m_knots[0].y + 2 * m_knots[1].y;
+    rhs[n - 1].y = (8 * m_knots[n - 1].y + m_knots[n].y) / 2.0;
+    rhs[0].x = m_knots[0].x + 2 * m_knots[1].x;
+    rhs[n - 1].x = (8 * m_knots[n - 1].x + m_knots[n].x) / 2.0;
+    // Get first control points X-values
+    std::vector<glm::vec3> ctrl = updateControlPoints(rhs);
+
+    // Set right hand side Y values
+
+    // Get first control points Y-values
+
+
+    // Fill output arrays.
+    for (size_t i = 0; i < n; ++i)
+    {
+        // First control point
+        m_firstControlPoints.emplace_back(glm::vec3(ctrl[i].x, ctrl[i].y,0));
+        // Second control point
+        if (i < n - 1)
+            m_secondControlPoints.emplace_back(glm::vec3(2 * m_knots[i + 1].x - ctrl[i + 1].x,
+                                                         2 *m_knots[i + 1].y - ctrl[i + 1].y,
+                                                         0));
+        else
+            m_secondControlPoints.emplace_back(glm::vec3((m_knots[n].x + ctrl[n - 1].x) / 2,
+                                                         (m_knots[n].y + ctrl[n - 1].y) / 2,
+                                                          0));
+    }
+
+}
+
 //! [5]
+std::vector<glm::vec3> TriangleWindow::updateControlPoints(std::vector<glm::vec3> & rhs){
+    size_t n = rhs.size();
+    std::vector<glm::vec3> solution(n); // Solution vector.
+    std::vector<glm::vec3> tmp(n); // Solution vector.
+
+    glm::vec3 b={2.0,2.0,0};
+    solution[0] = rhs[0] / b;
+    for (size_t i = 1; i < n; i++) // Decomposition and forward substitution.
+    {
+        tmp[i].x = 1 / b.x;
+        tmp[i].y = 1/ b.y;
+        b.x = (i < n - 1 ? 4.0 : 3.5) - tmp[i].x;
+        solution[i].x = (rhs[i].x - solution[i - 1].x) / b.x;
+        solution[i].y = (rhs[i].y - solution[i - 1].y) / b.y;
+
+    }
+    for (size_t i = 1; i < n; i++)
+        solution[n - i - 1] -= tmp[n - i] * solution[n - i]; // Backsubstitution.
+
+    return solution;
+}
